41bf420abe468882d59f26e404cfb0d47b59885d
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-09-23 13:18:05 +0500

    database connection is done

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,20 @@
+import mongoose, { connection } from "mongoose";
+
+async function dbConnect (){
+    if (connection.isConnected) {
+        console.log("Database is already connected");
+        return
+    }
+    try {
+       const db = mongoose.connect(process.env.MONGODB_URI )
+       connection.isConnected = await db.connections[0].readyState
+       console.log("DB connected successfully");
+       
+       
+    } catch (error) {
+        console.log("Database connection failed: ", error);
+        process.exit
+    }
+}
+
+export default dbConnect()
