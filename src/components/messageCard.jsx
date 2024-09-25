0e9b2524f4a14a936a67fbba319c570f093d3045
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-09-25 18:02:31 +0500

    cards added

--- src/components/messageCard.jsx
+++ src/components/messageCard.jsx
@@ -1,20 +1,68 @@
-"use client"
+"use client";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-  
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+  import { X } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import axios from "axios";
+import { response } from "express";
 
-const messageCard = () => {
+const messageCard = ({message, onMessageDelete}) => {
+
+    const {toast} = useToast()
+    const handleDeleteConfirm = async ()=> {
+      const response =  axios.delete(`/api/delete-message/${message._id}`)
+    }
+    toast({
+        title: response.data.message
+    })
+    onMessageDelete(message._id)
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <AlertDialog>
+  <AlertDialogTrigger>
+    <Button variant = "destructive"> <X className="w-5 h-5"/> </Button>
+  </AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your message
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction onClick = {handleDeleteConfirm}>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
 
-export default messageCard
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default messageCard;
