c89259d5765c9c577caa73ef29360b466e751dbf
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-09-25 19:15:57 +0500

    dashboard added

--- src/schemas/messageSchema.js
+++ src/schemas/messageSchema.js
@@ -1,6 +1,6 @@
 import {z} from "zod"
 
-export const signInSchema = z.object({
+export const messageSchema = z.object({
     content: z.string()
     .min(10, {message: "content must be atleast of 10 characters"})
     .max(300, {message: "content must be atmost of 300" })
